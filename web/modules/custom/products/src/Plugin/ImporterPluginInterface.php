9c9954eb2afa78e536d604f14e66560ef1f23808
juandlr <juanlr786@gmail.com>
2024-02-17 19:22:45 -0600

    product entity and configuration entity checkpoint 1

---
+++ web/modules/custom/products/src/Plugin/ImporterPluginInterface.php
@@ -0,0 +1,17 @@
+<?php
+namespace Drupal\products\Plugin;
+use Drupal\Component\Plugin\PluginInspectionInterface;
+/**
+ * Defines an interface for Importer plugins.
+ */
+interface ImporterPluginInterface extends
+  PluginInspectionInterface {
+  /**
+   * Performs the import.
+   *
+   * Returns TRUE if the import was successful or FALSE
+otherwise. *
+* @return bool
+   */
+  public function import();
+}
